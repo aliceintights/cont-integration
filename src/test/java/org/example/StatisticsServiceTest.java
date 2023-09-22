774f8e516a3cc2151d3d6aba9f8ae2e4a3665ce1
irina <obesnyuk@gmail.com>
2023-09-22 03:10:19 +0300

    new test

--- src/test/java/org/example/StatisticsServiceTest.java
+++ src/test/java/org/example/StatisticsServiceTest.java
@@ -18,43 +18,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
-
     @Test
-    void findMaxIfLower() {
+    void findMax2() {
         StatisticsService service = new StatisticsService();
 
-        long[] income = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long[] currentMax = {5, 8, 4, 5, 3, 8, 6, 11, 11};
+        long[] currentMax = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] income = {20, 50, 30, 90};
 
-        long expected = 11;
+        long expected = 12;
         long actual = service.findMax(currentMax);
 
         Assertions.assertEquals(expected, actual);
     }
-
     @Test
-    void findMaxIfIncomeLower() {
+    void findMax3() {
         StatisticsService service = new StatisticsService();
 
         long[] currentMax = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long[] income = {5, 8, 4, 5, 3, 8, 6, 11, 11};
+        long[] income = {20, 50, 30, 90};
 
-        long expected = 11;
+        long expected = 90;
         long actual = service.findMax(income);
 
         Assertions.assertEquals(expected, actual);
     }
-//
-//    @Test
-//    void findMaxIfEqual() {
-//        StatisticsService service = new StatisticsService();
-//
-//        long[] currentMax = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-//        long[] income = currentMax;
-//
-//        long expected = 12;
-//        long actual = service.findMax(income);
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
+    @Test
+    void findMax4() {
+        StatisticsService service = new StatisticsService();
+
+        long[] income = {20, 50, 30, 90};
+        long[] currentMax = income;
+
+        long expected = 90;
+        long actual = service.findMax(currentMax);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
