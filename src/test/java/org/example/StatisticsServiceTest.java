c450dd9bddef09f6caa65e8548885c7982e26ab2
irina <obesnyuk@gmail.com>
2023-09-24 19:29:42 +0300

    another test

--- src/test/java/org/example/StatisticsServiceTest.java
+++ src/test/java/org/example/StatisticsServiceTest.java
@@ -20,29 +20,12 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMin() {
-
+    void findMaxSecondTest() {
         StatisticsService service = new StatisticsService();
 
-        long[] currentMin = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long[] income = {20, 50, 30, 90};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 110, 11, 12};
 
         long expected = 12;
-        long actual = service.findMax(currentMin);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findMax2() {
-        StatisticsService service = new StatisticsService();
-
-        long[] currentMax = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long[] income = {20, 50, 30, 90};
-
-        long expected = 90;
-        long actual = service.findMax(income);
-
-        Assertions.assertEquals(expected, actual);
+        long actual = service.findMax(incomesInBillions);
     }
 }
