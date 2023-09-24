ae643cecc7dc30209755e418b9148501066cff1d
irina <obesnyuk@gmail.com>
2023-09-24 19:07:28 +0300

    new test

--- src/test/java/org/example/StatisticsServiceTest.java
+++ src/test/java/org/example/StatisticsServiceTest.java
@@ -18,6 +18,21 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+
+        StatisticsService service = new StatisticsService();
+
+        long[] currentMin = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] income = {20, 50, 30, 90};
+
+        long expected = 12;
+        long actual = service.findMax(currentMin);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void findMax2() {
         StatisticsService service = new StatisticsService();
@@ -25,32 +40,8 @@ public class StatisticsServiceTest {
         long[] currentMax = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long[] income = {20, 50, 30, 90};
 
-        long expected = 12;
-        long actual = service.findMax(currentMax);
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    void findMax3() {
-        StatisticsService service = new StatisticsService();
-
-        long[] currentMax = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long[] income = {20, 50, 30, 90};
-
         long expected = 90;
         long actual = service.findMax(income);
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    void findMax4() {
-        StatisticsService service = new StatisticsService();
-
-        long[] income = {20, 50, 30, 90};
-        long[] currentMax = income;
-
-        long expected = 90;
-        long actual = service.findMax(currentMax);
 
         Assertions.assertEquals(expected, actual);
     }
